b8d7759044ad22813db7dc24fc7b1c7a13397b84
Minyoung <alswn9781@naver.com>
2020-10-15 21:27:00 +0900

    Initial commit

---
+++ test.js
@@ -0,0 +1,14 @@
+$.ajax({
+    type: "GET",
+    url: "http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99",
+    data: {},
+    success: function (response) {
+      let cityAir = response["RealtimeCityAir"]["row"];
+      for (let i = 0; i < cityAir.length; i++) {
+        let mise = cityAir[i];
+        let guName = mise["MSRSTE_NM"];
+        let guMise = mise["PM10"];
+        console.log(guName, guMise);
+      }
+    },
+  });
